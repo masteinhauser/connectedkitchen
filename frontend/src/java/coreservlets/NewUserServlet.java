bda23831f412c307752191adef900b11bb33ba89
agilles2 <agilles220@gmail.com>
2011-12-01 19:02:04 -0600

    Added Login and New User Beans
    Will add to login and new user pages shortly.

--- frontend/src/java/coreservlets/NewUserServlet.java
+++ frontend/src/java/coreservlets/NewUserServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.*;
  *  to remember previous values.
  */
 
-public class RegistrationServlet extends HttpServlet {
+public class NewUserServlet extends HttpServlet {
   public void doGet(HttpServletRequest request,
                     HttpServletResponse response)
       throws ServletException, IOException {
