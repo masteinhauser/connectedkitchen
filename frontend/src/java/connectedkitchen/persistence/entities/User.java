8980489344979fb339b9faa96be4b07822942bef
Myles A. Steinhauser <myles.steinhauser@gmail.com>
2011-12-04 23:46:16 -0600

    Added PersistenceUnit and DataSource

--- frontend/src/java/connectedkitchen/persistence/entities/User.java
+++ frontend/src/java/connectedkitchen/persistence/entities/User.java
@@ -1,15 +1,21 @@
 package connectedkitchen.persistence.entities;
 
+import java.io.Serializable;
 import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.OneToMany;
-import org.hibernate.annotations.Entity;
 
 /**
  *
  * @author Myles A. K. Steinhauser <myles.steinhauser@gmail.com>
  */
 @Entity
-public class User extends BaseEntity {
+@Table(name = "user")
+@SequenceGenerator(name="user_seq",sequenceName="user_id_seq")
+public class User extends BaseEntity implements Serializable {
     
     @OneToMany(mappedBy="user")
     private Set<Favorite> favorites;
@@ -23,4 +29,21 @@ public class User extends BaseEntity {
     @OneToMany(mappedBy="user")
     private Set<Inventory> inventory;
     
+    private Integer id;
+    private boolean admin;
+    
+    public User(){}
+    
+    public User(Integer id, boolean admin){
+        this.id = id;
+        this.admin = admin;
+    }
+    
+    public Integer getUserId(){
+        return id;
+    }
+    
+    public boolean getAdmin(){
+        return admin;
+    }
 }
